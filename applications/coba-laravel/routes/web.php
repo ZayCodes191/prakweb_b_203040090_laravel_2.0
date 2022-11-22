712590ad7cd5c756ead9836d48570b264f5f882e
ZayCodes191 <203040090@mail.unpas.ac.id>
2022-11-22 14:59:11 +0700

    Menyelesaikan Video 23

--- applications/coba-laravel/routes/web.php
+++ applications/coba-laravel/routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -65,4 +66,9 @@ Route::get('/dashboard', function() {
 
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSLug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
+
+
+// 
 
